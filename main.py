a368473fd3e58654c5c6d44c8512959205a74d3c
drt9739 <grigchita@gmail.com>
2023-01-09 18:52:52 +0900

    Немного исправлений в комментах

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import os
 import sys
 import time
 
-
-"""Инициализация величин"""
-gravity_acceleration = 10 # Ускорение свободного падения
+# Инициализация величин
+gravity_acceleration = 10  # Ускорение свободного падения
 
 cell_size = 60
 
@@ -32,11 +31,10 @@ class Player:
         gravity_acceleration = 10
 
 
-
-"""Доска"""
+# Класс доски
 class Board:
     # Инициализация
-    def __init__(self, size, width, height): # Принимает размер клетки, ширину и высоту доски в клетках
+    def __init__(self, size, width, height):  # Принимает размер клетки, ширину и высоту доски в клетках
         self.tiles = pygame.sprite.Group()
         self.size = size
         self.width = width
@@ -48,7 +46,6 @@ class Board:
             for j in i:
                 print(j)
 
-
     def build(self, pattern):
         self.map = pattern
         for i in self.map:
@@ -56,14 +53,15 @@ class Board:
                 print(j)
 
     # Отрисовка доски (сетка времена)
-    def render(self, screen): # Принимает экран для отрисовки
+    def render(self, screen):  # Принимает экран для отрисовки
         self.tiles.draw(screen)
 
 
-"""Клетки (объекты)"""
+# Клетки (объекты)
 class Tile:
     # Инициализация
-    def __init__(self, board, type, collision_level, sprite, global_x, global_y): # Принимает тип клетки, уровень коллизии*, спрайт (открытый файл)
+    def __init__(self, board, type, collision_level, sprite, global_x,
+                 global_y):  # Принимает тип клетки, уровень коллизии*, спрайт (открытый файл)
         global cell_size
         self.type = type
         self.collision_level = collision_level
@@ -75,7 +73,8 @@ class Tile:
         self.sprite.rect.x = global_x * cell_size + 210
         self.sprite.rect.y = global_y * cell_size + 150
 
-def open_image(name, width, height): #
+
+def open_image(name, width, height):  #
     if name:
         full_path = os.path.join('Data/sprites', name)
         if os.path.isfile(full_path):
@@ -84,37 +83,38 @@ def open_image(name, width, height): #
             return image
     return None
 
-"""Программа"""
+
+# главный цикл
 def main():
-    """Инициализация процесса"""
-    TPS = 60 # Обновления (tick) в секунду
+    # инициализация проекта
+    TPS = 60  # Обновления (tick) в секунду
     pygame.init()
     board = Board(cell_size, 25, 12)
     pattern = [[[[0]] * 25] * 11, [[[Tile(board, "ground", 20, "ground.png", 0, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 1, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 2, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 3, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 4, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 5, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 6, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 7, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 8, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 9, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 10, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 11, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 12, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 13, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 14, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 15, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 16, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 17, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 18, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 19, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 20, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 21, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 22, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 23, 12)],
-                                   [Tile(board, "ground", 20, "ground.png", 24, 12)]]]]
+                                    [Tile(board, "ground", 20, "ground.png", 1, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 2, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 3, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 4, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 5, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 6, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 7, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 8, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 9, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 10, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 11, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 12, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 13, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 14, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 15, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 16, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 17, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 18, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 19, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 20, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 21, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 22, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 23, 12)],
+                                    [Tile(board, "ground", 20, "ground.png", 24, 12)]]]]
     board.build(pattern)
     pygame.display.set_caption("It's time for bullet hell")
     size = width, height = 1920, 1080
@@ -141,12 +141,10 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-"""
- Примечания:
-    • Уровень коллизии - целое число, необходимое для определения возможности проникновения определённого предмета 
-      через клетку (чем выше, тем сложнее)
-    • Уровень проникновение - целое число, определяющее способность предмета проходить через клетку
-    • Глобальная позиция - позиция относительно доски
-    • Локальная позиция - позиция относительно клетки
-"""
+# Примечания:
+#   • Уровень коллизии - целое число, необходимое для определения возможности проникновения определённого предмета
+#     через клетку (чем выше, тем сложнее)
+#   • Уровень проникновение - целое число, определяющее способность предмета проходить через клетку
+#   • Глобальная позиция - позиция относительно доски
+#   • Локальная позиция - позиция относительно клетки
+#
