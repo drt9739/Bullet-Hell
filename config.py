2a9ee0eafd3d907bff682db866d16977372ea4a9
drt9739 <grigchita@gmail.com>
2023-01-17 00:53:49 +0900

    issues #16 - упращаю жизнь

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 cell_size = 60
 gravity_acceleration = 10
 TPS = 60  # Обновления (tick) в секунду
-width, height = 1920, 1080
+width, height = 720, 720
+level_path = ''
--- main.py
+++ main.py
@@ -3,46 +3,69 @@ import os
 import sys
 import time
 
+import config
 from Scripts.Class.player import Player
 from Scripts.Class.layout import Layout
 from Scripts.Class.block import Block
 from Scripts.open_image import open_image
 from config import cell_size, TPS, width, height
+from Scripts.load_level import load_level
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+class Menu:
+    font_image = ''
+
+    def __init__(self, surface: object):
+        self.surface = surface
+
+        self.run()
+
+    def run(self):
+        if self.font_image:
+            font_img = open_image(self.font_image, width, height)
+            screen.blit(font_img, (0, 0))
+
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        intro_text = ['говно', 'запупа']
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('white'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            screen.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                    return
+            pygame.display.flip()
+            clock.tick(config.TPS)
+
+
+class Game:
+    pass
 
 
 def main():
     # инициализация проекта
-    pygame.init()
+    Menu(screen)
+
     board = Layout(cell_size, 25, 12)
-    pattern = [[[[0]] * 25] * 11, [[[Block(board, "ground", 20, "ground.png", 0, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 1, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 2, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 3, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 4, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 5, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 6, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 7, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 8, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 9, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 10, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 11, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 12, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 13, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 14, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 15, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 16, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 17, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 18, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 19, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 20, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 21, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 22, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 23, 12)],
-                                    [Block(board, "ground", 20, "ground.png", 24, 12)]]]]
+    pattern = load_level('level.data', board)
     board.build(pattern)
     pygame.display.set_caption("It's time for bullet hell")
     size = width, height
-    screen = pygame.display.set_mode(size)
+
     player = Player(500, 500)
     running = True
     while running:
@@ -63,6 +86,12 @@ def main():
 
 
 if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+    clock = pygame.time.Clock()
+
+    if config.level_path == '':
+        config.level_path = os.path.abspath('data')
     main()
 
 # Примечания:
